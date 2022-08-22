64fbf2c1770dfb9ca2384de35a6879bda66a38e9
Junrey Lañas <jrydev404@gmail.com>
2022-08-23 05:32:52 +0800

    queryCache, staleTime, reFetch, polling

--- src/components/DataFetchRQ.jsx
+++ src/components/DataFetchRQ.jsx
@@ -5,14 +5,24 @@ import axios from "axios";
 const getUsers = () => axios.get('http://localhost:8000/users')
 
 const DataFetchRQ = () => {
-    const { isLoading, data, error } = useQuery('users', getUsers)
-    if (isLoading) <h1>Loading...</h1>
+    const { isLoading, data, error, isFetching, refetch } = useQuery('users', getUsers, {
+        // staleTime: 30000,
+        // refetchOnMount: true,
+        // refetchOnWindowFocus: 'always',
+        // refetchInterval: 1000,
+        // refetchIntervalInBackground: true,
+
+        enabled: false,
+    })
+
+    if (isLoading || isFetching) <h1>Loading...</h1>
     if (error) <h1>Error...</h1>
 
 
     return (
         <>
             <h1>This is Data Fetch using RQ</h1>
+            <button onClick={refetch}>Fetch data</button>
             {data?.data.map(users => <h3 key={users.id}>{users.name}</h3>)}
         </>
     )
